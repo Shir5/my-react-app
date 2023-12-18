fd854f12027bc2d4e4949e717a7dcf1a7c33081d
Shir5 <Vova0120_black@icloud.com>
2023-12-18 16:05:28 +0300

    v 0.1

--- src/Components/HeaderComponents/Logo.jsx
+++ src/Components/HeaderComponents/Logo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import classes from "./Logo.module.scss";
+
 
 const Logo = function (){
     return (
         <div>
-            <img src="Resources/EazyBox_Logo.svg" alt="Logo" className={classes.logo}/>
+            <img src="Resources/EazyBox_Logo.svg" alt="Logo" />
         </div>
     );
 }
