9d1ad33ebceca1d2083aea01c854dfbce4c062a2
Shir5 <Vova0120_black@icloud.com>
2023-12-18 12:04:06 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "./HeaderComponents/Logo";
+
+
+const Header = function (){
+    return (
+        <div>
+            <Logo />
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Components/HeaderComponents/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Logo = function (){
+    return (
+        <div>
+            <img src="Resources/EazyBox_Logo.svg" alt="Logo" />
+        </div>
+    );
+}
+
+export default Logo;
