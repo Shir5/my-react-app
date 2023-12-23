92f65104839efdcf75606a6c9f1b4524d2fcc4f1
Shir5 <Vova0120_black@icloud.com>
2023-12-23 19:20:45 +0300

    theme changer

--- src/themeChanger/ThemeChanger.jsx
+++ src/themeChanger/ThemeChanger.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ThemeChangerStyles.scss";
 
 const ThemeChanger = function () {
+    const [themeSrc, setThemeSrc] = useState("/Resources/icons8-moon-30.svg");
+    const htmlElement = document.querySelector('html');
+
+    function changeTheme() {
+        if (themeSrc === "/Resources/icons8-moon-30.svg") {
+            setThemeSrc("/Resources/icons8-sun.svg");
+            htmlElement.setAttribute('data-theme', 'light');
+        } else {
+            setThemeSrc("/Resources/icons8-moon-30.svg");
+            htmlElement.setAttribute('data-theme', 'dark');
+        }
+    }
+
     return (  
         <div>
-            
+            <button className="themeButton" onClick={changeTheme}>
+                <img src={themeSrc} alt="Logo" className="themeImage" />
+            </button>
         </div>
     );
 }
